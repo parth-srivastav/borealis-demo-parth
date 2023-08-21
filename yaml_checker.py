208d4fa5b2fb60ba80b6fa178c86cda748543513
parth <parth.srivastav@armory.io>
2023-08-21 12:57:46 -0700

    add print message for debug

--- yaml_checker.py
+++ yaml_checker.py
@@ -21,6 +21,7 @@ if __name__ == "__main__":
     account = sys.argv[2]
     application_name = sys.argv[3]
     if check_yaml(yaml_path, account, application_name):
+        print ("Combination Found")
         sys.exit(1)
     else:
         sys.exit(0)
