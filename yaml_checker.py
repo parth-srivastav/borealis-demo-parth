3396ac12d41116184f3ae9c3a13c1accba71e62d
parth <parth.srivastav@armory.io>
2023-08-21 12:15:45 -0700

    add arguments for blockcheck

--- yaml_checker.py
+++ yaml_checker.py
@@ -1,7 +1,7 @@
 import yaml
 import sys
 
-def check_yaml(yaml_path):
+def check_yaml(yaml_path, account, application_name):
     with open(yaml_path, 'r') as yaml_file:
         data = yaml.safe_load(yaml_file)
 
@@ -10,7 +10,7 @@ def check_yaml(yaml_path):
     for item in block_deployment:
         if (
             'application' in item and 'accounts' in item and
-            'potato-lies' in item['application'] and 'prod-west' in item['accounts']
+            application_name in item['application'] and account in item['accounts']
         ):
             return True
     
@@ -18,7 +18,9 @@ def check_yaml(yaml_path):
 
 if __name__ == "__main__":
     yaml_path = sys.argv[1]
-    if check_yaml(yaml_path):
+    account = sys.argv[2]
+    application_name = sys.argv[3]
+    if check_yaml(yaml_path, account, application_name):
         print("The specified combination exists in the YAML file.")
     else:
         print("The specified combination does not exist in the YAML file.")
