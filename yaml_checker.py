54ec43c50936d78f4d971f7869db5822133a6765
parth <parth.srivastav@armory.io>
2023-08-28 13:28:06 -0700

    added generic exception handling

--- yaml_checker.py
+++ yaml_checker.py
@@ -2,20 +2,24 @@ import yaml
 import sys
 
 def check_yaml(yaml_path, account, application_name):
-    with open(yaml_path, 'r') as yaml_file:
-        data = yaml.safe_load(yaml_file)
+    try:
+        with open(yaml_path, 'r') as yaml_file:
+            data = yaml.safe_load(yaml_file)
 
-    block_deployment = data.get('blockDeployment', [])
+        block_deployment = data.get('blockDeployment', [])
 
-    for item in block_deployment:
-        if (
-            'application' in item and 'accounts' in item and
-            (application_name in item['application'] or '*' in item['application']) and (account in item['accounts'] or '*' in item['accounts'])
-        ):
-            return True
+        for item in block_deployment:
+            if (
+                'application' in item and 'accounts' in item and
+                (application_name in item['application'] or '*' in item['application']) and (account in item['accounts'] or '*' in item['accounts'])
+            ):
+                return True
+        
+        return False
+    except:
+        print ("Exception Occurred, allowing deployments")
+        return False
     
-    return False
-
 if __name__ == "__main__":
     yaml_path = sys.argv[1]
     account = sys.argv[2]
